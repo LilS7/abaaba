aa43f4ad9b095fd5d585334e97e49b0b7d29f28d
leeshi <shili10001@gmail.com>
2020-11-30 15:13:30 +0800

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='abaaba',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      test_suite = 'tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/abaaba-run'],
+      zip_safe=False)
